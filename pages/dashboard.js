737c4a5975a8dffccc6254b0d419de9aa6e6fd63
cdthomp1 <camthomp96@gmail.com>
2021-08-27 22:42:33 -0600

    Clean up

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,31 +1,9 @@
-import React, { useState } from 'react';
-import useSWR from 'swr'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDumpster, faEdit } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
 import FilamentTable from '../components/FilamentTable';
-import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 import PrintsTable from '../components/PrintsTable';
-
-const fetcher = async (url) => {
-    const res = await fetch(url)
-    const data = await res.json()
-
-    if (res.status !== 200) {
-        throw new Error(data.message)
-    }
-    return data
-}
-
+import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 
 export default withPageAuthRequired(function dashboard({ user }) {
-
-
-    const { data: filaments, error: filamentsError } = useSWR(`/api/filament/getFilaments?id=${user.sub}`, fetcher)
-
-
-    if (filamentsError) return <div>{filamentsError.message}</div>
-    if (!filaments) return <div>Loading...</div>
-
     return (
         <>
             <div className="flex flex-col items-center w-full mt-6">
