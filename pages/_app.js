c32cd4e920fb740369af154cd199c238ba2bd5ae
cdthomp1 <camthomp96@gmail.com>
2021-09-08 14:47:34 -0600

    coming soon

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 import 'tailwindcss/tailwind.css'
-import Navigation from '../components/Navigation';
 
-import { UserProvider } from '@auth0/nextjs-auth0';
+
+
 
 function MyApp({ Component, pageProps }) {
   return (
-    <UserProvider>
-      <Navigation />
-      <Component {...pageProps} />
-    </UserProvider>
+    <Component {...pageProps} />
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,10 @@ import { faDumpster } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center py-2">
-      <h1 className="2xl:text-6xl xl:text-6xl lg:text-5xl md:text-5xl sm:text-4xl text-4xl mt-10">Filament Tracker</h1>
+    <div className="flex flex-col items-center justify-center h-screen text-center text-white bg-regal-blue">
+      <h1 className="2xl:text-6xl xl:text-6xl lg:text-5xl md:text-5xl sm:text-4xl text-4xl">Filament Hero</h1>
+      <h2 className="2xl:text-5xl xl:text-5xl lg:text-4xl md:text-4xl sm:text-3xl text-3xl">Coming Soon!</h2>
+      <p>Follow <a className="text-blue-500" href="https://twitter.com/FilamentHero" target="_blank" rel="noopener noreferrer">@FilamentHero</a> on Twitter for updates!</p>
     </div>
   )
 }
