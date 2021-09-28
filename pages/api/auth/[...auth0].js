5dd331c7eb770aff29e4a22ccc7d982a7d07710b
cdthomp1 <camthomp96@gmail.com>
2021-09-27 23:41:08 -0600

    try staging

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,3 +1,4 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+import loginHandlerFactory from '@auth0/nextjs-auth0/dist/auth0-session/handlers/login';
 
 export default handleAuth();
--- pages/index.js
+++ pages/index.js
@@ -10,17 +10,9 @@ export default function Home() {
           <h1 className="2xl:text-6xl xl:text-6xl lg:text-5xl md:text-5xl sm:text-4xl text-6xl mb-3">Filament<br />Hero</h1>
           <hr className="border-0 bg-black text-gray-500 h-0.5" />
           <h2 className="2xl:text-4xl xl:text-4xl lg:text-4xl md:text-4xl sm:text-3xl text-5xl mb-3 mt-3">Your 3D Printing<br />Companion</h2>
-          <div className="flex justify-between w-96 mt-6">
-            <a href="#learnMore"  ><div className="text-lg font-medium bg-gray-500 text-white px-8 py-2 rounded-lg shadow hover:bg-purple-700 transition-color duration-100 ease-in-out active:shadow-outline focus:shadow-outline">Learn More</div></a>
-            <div className="text-lg font-medium bg-regal-blue text-white px-8 py-2 rounded-lg shadow hover:bg-purple-700 transition-color duration-100 ease-in-out active:shadow-outline focus:shadow-outline"><a href="/api/auth/login">Get Started</a></div>
+          <div className="flex justify-center w-96 mt-6">
+            <div className="text-lg font-medium bg-regal-blue text-white px-8 py-2 rounded-lg shadow hover:bg-purple-700 transition-color duration-100 ease-in-out active:shadow-outline focus:shadow-outline"><a href="/dashboard">Get Started</a></div>
           </div>
-        </div>
-      </div>
-      <div className="flex flex-col items-center justify-center" id="learnMore">
-        <h2 className="2xl:text-4xl xl:text-4xl lg:text-4xl md:text-4xl sm:text-3xl text-5xl mb-3 mt-3">Keep Track of Filament</h2>
-        <h2 className="2xl:text-4xl xl:text-4xl lg:text-4xl md:text-4xl sm:text-3xl text-5xl mb-3 mt-3">The Easy Way</h2>
-        <div>
-      
         </div>
       </div>
     </>
