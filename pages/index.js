e04a5685330df9864b975fa512f750c3f515542e
cdthomp1 <camthomp96@gmail.com>
2021-10-19 13:23:52 -0600

    Added discord link

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,7 @@ export default function Home() {
         <h1 className="2xl:text-6xl xl:text-6xl lg:text-5xl md:text-5xl sm:text-4xl text-4xl">Filament Hero</h1>
         <h2 className="2xl:text-5xl xl:text-5xl lg:text-4xl md:text-4xl sm:text-3xl text-3xl">Coming Soon!</h2>
         <p>Follow <a className="text-blue-500" href="https://twitter.com/FilamentHero" target="_blank" rel="noopener noreferrer">@FilamentHero</a> on Twitter for updates!</p>
+        <p>Join the Official Discord to be apart of the development process:<br /><a className="text-blue-500" href="https://discord.gg/jKAAhGxyhF" target="_blank" rel="noopener noreferrer">Invite Link</a></p>
       </div>
     </>
   )
