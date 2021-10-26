23e670e5435de5e4c101a98037676897894c7afb
cdthomp1 <camthomp96@gmail.com>
2021-10-25 22:30:33 -0600

    Add user id

--- pages/api/filament/createFilament.js
+++ pages/api/filament/createFilament.js
@@ -19,6 +19,7 @@ export default async (req, res) => {
         purchaseLocation: jsonBody.purchaseLocation,
         generalNotes: jsonBody.generalNotes,
         picture: jsonBody.picture,
+        userId: jsonBody.userId
     });
     const createdFilament = await newFilament.save()
     res.status(200).json(createdFilament)
