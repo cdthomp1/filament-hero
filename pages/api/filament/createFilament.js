f655b6ae0fceea6a4050489ba487cceba603b891
cdthomp1 <camthomp96@gmail.com>
2021-11-02 21:17:43 -0600

    Add brand

--- pages/api/filament/createFilament.js
+++ pages/api/filament/createFilament.js
@@ -1,10 +1,11 @@
 import Filament from '../models/Filament';
-import connectDB from  "../../../lib/connectDb";
+import connectDB from "../../../lib/connectDb";
 
 export default async (req, res) => {
     await connectDB();
     const jsonBody = JSON.parse(req.body);
     const newFilament = new Filament({
+        brand: jsonBody.brand,
         type: jsonBody.type,
         color: jsonBody.color,
         length: jsonBody.length,
