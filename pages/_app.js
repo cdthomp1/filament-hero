6684d98f4618a915e1f26735133eade7bf1b0377
cdthomp1 <camthomp96@gmail.com>
2021-11-12 14:49:44 -0700

    Add toast container

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,22 @@
 import 'tailwindcss/tailwind.css'
 import Navigation from '../components/layout/Navigation';
-
 import { UserProvider } from '@auth0/nextjs-auth0';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
       <Navigation />
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        hideProgressBar={true}
+        newestOnTop={false}
+        rtl={false}
+        draggable
+      />
       <Component {...pageProps} />
     </UserProvider>
   );
