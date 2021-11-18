51d917b77ec5305d2e6aa7f63f763e8ab3146f5d
cdthomp1 <camthomp96@gmail.com>
2021-11-17 20:44:20 -0700

    Remove white space

--- pages/api/filament/deleteFilament.js
+++ pages/api/filament/deleteFilament.js
@@ -1,5 +1,5 @@
 import Filament from '../models/Filament';
-import connectDB from  '../../../lib/connectDb';
+import connectDB from '../../../lib/connectDb';
 
 export default async (req, res) => {
     await connectDB();
