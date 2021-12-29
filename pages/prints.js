3d88b70e6330a2514b8339c55284d40a59ae712d
Cameron Thompson <camthomp96@gmail.com>
2021-12-28 20:19:23 -0700

    link update

--- pages/prints.js
+++ pages/prints.js
@@ -24,7 +24,7 @@ export default withPageAuthRequired(function print({ user }) {
             <div className="flex flex-col items-center">
                 <h3 className="text-2xl text-center p-6">&#x1F6A8; You need to add a Printer and a Filament before you can add a print! &#x1F6A8;</h3>
                 <div className="m-6">
-                    <Link href="/addPrinter"><button className="p-2 pl-5 pr-5 bg-transparent border-2 border-purple-500 text-purple-500 text-lg rounded-lg transition-colors duration-300 transform hover:bg-purple-500 hover:text-gray-100 focus:border-4 focus:border-purple-300">Add a Printer!</button></Link>
+                    <Link href="/printers"><button className="p-2 pl-5 pr-5 bg-transparent border-2 border-purple-500 text-purple-500 text-lg rounded-lg transition-colors duration-300 transform hover:bg-purple-500 hover:text-gray-100 focus:border-4 focus:border-purple-300">Add a Printer!</button></Link>
                 </div>
             </div>
         </>)
