071a8034c21fcccab1fbbe741c20d3fb6ea95f3d
cdthomp1 <camthomp96@gmail.com>
2022-06-02 23:45:24 -0600

    Added footer and update prints page

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import 'tailwindcss/tailwind.css'
 import Navigation from '../components/layout/Navigation';
 import Toast from '../components/layout/Toast';
+import Footer from '../components/layout/Footer';
 import { UserProvider } from '@auth0/nextjs-auth0';
 
 function MyApp({ Component, pageProps }) {
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }) {
       <Navigation />
       <Toast />
       <Component {...pageProps} />
+      <Footer />
     </UserProvider>
   );
 }
