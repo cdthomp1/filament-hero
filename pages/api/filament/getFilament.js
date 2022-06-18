5370066bc43c458fd09c1ca912a45612c4f3d93f
cdthomp1 <camthomp96@gmail.com>
2022-06-17 20:39:06 -0600

    More dev, not quite done. Actually far from done

--- pages/api/filament/getFilament.js
+++ pages/api/filament/getFilament.js
@@ -21,9 +21,9 @@ export default async (req, res) => {
         const client = await clientPromise
         const db = client.db("filamenttracker")
         var id = req.query.id
-        const userData = await db.collection('users').findOne({ 'filaments.id': ObjectId(id) })
-
-        let filament = userData.filaments.find(f => f.id.toString() === id)
+        const userData = await db.collection('users.filaments').findOne({ '_id': ObjectId(id) })
+        console.log(userData)
+        /* let filament = userData.filaments.find(f => f.id.toString() === id) */
 
         res.status(200).json(filament);
 
