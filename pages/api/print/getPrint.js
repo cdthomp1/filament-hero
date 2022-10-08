330a3f66f8bf714542ca4543755bad40872d6540
cdthomp1 <camthomp96@gmail.com>
2022-10-08 13:56:22 -0600

    Finish the get print api

--- pages/api/print/getPrint.js
+++ pages/api/print/getPrint.js
@@ -1,31 +1,28 @@
-/* const mongoose = require('mongoose');
-const Print = require('../models/Print')
+import clientPromise from "../../../lib/connectDb"
+import { ObjectId } from "mongodb"
 
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-            useCreateIndex: true
-        })
-
-    } catch (err) {
-        process.exit(1);
-    }
-}
 
 export default async (req, res) => {
 
+    try {
+        const client = await clientPromise;
+        const db = client.db("filamenttracker");
 
-    const id = req.query.id
+        var id = req.query.printId
+        if (!id) {
+            res.status(404).json({ message: "No print ID provided 😩" });
+        }
 
-    const foundPrint = await Print.findById(id)
+        const print = await db.collection('prints').findOne({ '_id': ObjectId(id) });
 
-    if (foundPrint) {
-        res.status(200).json(foundPrint)
-    } else {
-        res.status(404).json({ message: "Print not found 😩" })
+        if (print) {
+            res.status(200).json(print);
+        } else {
+            res.status(404).json({ message: "Print not found 😩" });
+        }
+
+    } catch (error) {
+        console.error(e)
+        res.status(500).json({ isConnected: false });
     }
-
-
-} */
+}
